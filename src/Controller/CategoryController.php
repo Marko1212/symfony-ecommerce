36984d916e3dc6c753d14d6faedbad6920db27f6
Hemjie <marjolaine.vilain@gmail.com>
2021-01-28 15:08:02 +0100

    page des produits d'une catégorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @Route("/category/{category}", name="category_show")
      */
-    public function index(): Response
+     public function show(Category $category): Response
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        return $this->render('category/show.html.twig', [
+            'products' => $category->getProduct(),
         ]);
     }
 }
