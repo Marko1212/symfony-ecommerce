c7e49e338994ba05d65a2c1b62ea37369582f6e1
unknown <geoffreyparle@gmail.com>
2021-01-28 15:46:45 +0100

    fix bug

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category/{category}", name="category_show")
+     * @Route("/category/{option}", name="category_show")
      */
-     public function show(Category $category): Response
+     public function show(ProductRepository $repository, $option, CategoryRepository $categoryRepository): Response
     {
+        $category = $categoryRepository->findOneBy(['name' => $option]);
+        
+        dump($category->getId());
+        $products = $repository->findBy([ 'category' => $category->getId()]);
+        dump($products);
+
         return $this->render('category/show.html.twig', [
-            'products' => $category->getProduct(),
+            'products' => $products,
         ]);
     }
 }
