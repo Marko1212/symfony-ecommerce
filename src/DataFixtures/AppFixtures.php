1a34faebf53ebb2c70affd7cb966bb8c07308e42
Hemjie <marjolaine.vilain@gmail.com>
2021-01-28 08:50:52 +0100

    installation fixtures + fixtures des catégories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,24 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // On crée une instance de Faker pour générer la donnée aléatoire
+        $faker = Factory::create('fr_FR');
+        $categoryNames = ['multimédia', 'jouets pour animaux', 'bricolage', 'jardinage', 'papeterie'];
+        foreach($categoryNames as $index => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $this->addReference('cat-'.$index, $category);
+            $manager->persist($category);
+        }
 
         $manager->flush();
     }
