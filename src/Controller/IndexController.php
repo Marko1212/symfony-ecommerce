e2c7a0a74cd5091c6faf6ac556317b7bec8eb6dc
Marko1212 <shihonage7@gmail.com>
2021-01-30 14:19:16 +0100

    ajouté filtres par couleurs sur page products et pages catégory

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Entity\Review;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +14,25 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $repository = $this->getDoctrine()->getRepository(Product::class);
+        $aleaProducts = $repository->findAleasProducts();
+
+        $aleaCrush = $repository->findOneAleaCrush();
+
+        $lastProducts = $repository->findlastProducts();
+
+        $repoReview = $this->getDoctrine()->getRepository(Review::class);
+        $bestProducts = $repoReview->findBestProducts();
+
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'aleaProducts' => $aleaProducts,
+            'aleaCrush' => $aleaCrush[0],
+            'lastProducts' => $lastProducts,
+            'bestProducts' => $bestProducts,
         ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,17 +16,24 @@ class ProductController extends AbstractController
     /**
      * @Route("/products", name="product_list")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
         $repository = $this->getDoctrine()->getRepository(Product::class);
 
-        $products = $repository->findAll();
+        $products =$repository->findAll();
         $lastProduct = end($products);
+
+       // dump($request->get('colors'));
+        if (!empty($request->get('colors'))) {
+
+            $products = $repository->findAllWithFilters(
+                $request->get('colors')
+            );
+        }
 
         $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
         $categories = $categoryRepository->findAll();
-
 
         return $this->render('product/list.html.twig', [
             'products' => $products,
