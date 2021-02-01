b25df79b98feaacb693e4204145b24aad73ad3a9
unknown <geoffreyparle@gmail.com>
2021-02-01 08:29:52 +0100

    debut contact

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Review;
+use App\Form\ReviewType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -38,7 +41,7 @@ class ProductController extends AbstractController
     /**
      * @Route("/product/{slug}_{id}", name="product_show", requirements={"slug"="[a-z0-9\-]*"})
      */
-    public function show(Product $product)
+    public function show(Product $product, Request $request, $id)
     {
         $countReview = count($product->getReviewsList());
         $mark = 0;
@@ -46,12 +49,20 @@ class ProductController extends AbstractController
             $mark += $product->getReviewsList()[$i]->getMark();
         }
         $mark = $mark / $countReview;
+
+        $review = new Review();
+        $formReview = $this->createForm(ReviewType::class, $review);
+        $formReview->handleRequest($request);
+        
+        
+
      
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
             'countReview' => $countReview,
             'mark' => $mark,
+            'formReview' => $formReview->createView(),
         ]);
     }
     
---
+++ src/Form/ReviewType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom d\'utilisateur est obligatoire'
+                    ]),
+                ],
+            ])
+            ->add('mark', ChoiceType::class, [
+                'label' => 'Note',
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+            ])
+            ->add('comment', null, [
+                'label' => 'Message',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom d\'utilisateur est obligatoire'
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Review::class,
+        ]);
+    }
+}
